5aef82267c077ba544326b405babec0bf7cab834
roldie9 <aroldoreyes9@gmail.com>
2020-03-05 22:44:20 -0600

    added module exports to html function

--- Master/server.js
+++ Master/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require ("path");
 
 //initializes the app and creates a port
 const app = express();
@@ -10,13 +11,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"))
 
 //routes that send user to correct html pages
-app.get("/notes", function(res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
-});
+module.exports = function(app) {
+    app.get("/notes", function(res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
 
-app.get("*", function(res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
+    app.get("*", function(res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+};
+
+//
 
 //starts the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
